c16e1865c6ec2211ab58c9e9451df2d095a52a93
SuckMyLeg <juanito-146@hotmail.es>
2022-01-01 21:36:03 +0100

    Saving

--- players_writer.js
+++ players_writer.js
@@ -1,6 +1,7 @@
 
-function write(data){
-    var screen = document.getElementById("");
+function write(player){
+    var screen = document.getElementById("screen");
 
+    screen.innerHTML = '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="imag" src="MIMIKYUG.png"></div><img class="imag" src="Images/Hearts/'+player.healt+'.png"></div>';
 
 }
