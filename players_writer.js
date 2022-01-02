cac20faf508baa5ee2d6176f8dc9659d53b64db4
SuckMyLeg <juanito-146@hotmail.es>
2022-01-02 10:49:40 +0100

    Saving

--- players_writer.js
+++ players_writer.js
@@ -2,6 +2,6 @@
 function write(player){
     var screen = document.getElementById("screen");
 
-    screen.innerHTML = '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="imag" src="MIMIKYUG.png"></div><img class="imag" src="Images/Hearts/'+player.healt+'.png"></div>';
+    screen.innerHTML = screen.innerHTML + '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" src="Images/Hearts/'+player.healt+'.png"></div>';
 
 }
