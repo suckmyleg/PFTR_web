0ca1275ef230f35f6cb0816b08c0adad47b06e07
SuckMyLeg <juanito-146@hotmail.es>
2022-01-03 04:26:19 +0100

    Saving

---
+++ DATABASES/PFTR.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `matches` (
+  `id` INT(6) PRIMARY KEY AUTO_INCREMENT,
+  `server_name` VARCHAR(20),
+  `winner` VARCHAR(20),
+  `players` VARCHAR(500),
+  `max_players` INT(4) DEFAULT 20,
+  `players_data` VARCHAR(2000),
+  `added` TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `servers` (
+  `id` INT(6) PRIMARY KEY AUTO_INCREMENT,
+  `server_name` VARCHAR(20),
+  `key` VARCHAR(20),
+  `added` TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+);
--- api.php
+++ api.php
@@ -1,5 +1,16 @@
 <?php
 
+$servername = "localhost";
+$username = "PFTR";
+$password = "PFTR_PASSWORD_PFTR";
+$db = "pftr";
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
 function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -12,40 +23,159 @@ function generateRandomString($length = 10) {
 
 function verifyKeyFromServer($name, $key)
 {
-	return getKeyFromServer($name) == $key;
+    $key_ =getKeyFromServer($name);
+    if ($key){return FALSE;}
+	return $key_ == $key;
 }
 
 function getKeyFromServer($name)
 {
-	return "";
+       $sql = "SELECT key FROM servers WHERE name='" + $name + "'";
+       $result = $conn->query($sql);
+
+       if ($result->num_rows > 0) {
+         // output data of each row
+         while($row = $result->fetch_assoc()) {
+           if($key == ""){return FALSE;}
+           return $key;
+         }
+       } else {
+         return FALSE ;
+       }
 }
 
-function save_to_database($name, $key, $content)
+function save_to_database($name, $key, $match_id, $content)
 {
-	if (verifyKeyFromServer($name, $key))
+	if ($permission)
 	{
-
-		return "Saved";
+        $sql = "UPDATE matches SET players_data='"+$content+"' WHERE id='" + $match_id + "'";
+        if ($conn->query($sql) === TRUE) {
+            return "OK";
+        } else {
+          die("Error updating: " . $conn->error);
+        }
 	}
 	else
 	{
-		return "Permission denied";
+		die("Permission denied");
 	}
 }
 
-function host_new_match()
+function host_new_match($name, $key, $players, $max_players)
 {
+    if ($permission)
+    	{
+            $sql = "INSERT INTO matches (server_name, winner, players, max_players, players_data)
+                   VALUES ('"+$name+"', '', '"+$players+"', '"+$max_players+"', '[]')";
+
+            if ($conn->query($sql) === TRUE)
+            {
+                return "OK";
+            }
+            else
+            {
+                die("Error: " . $sql . "<br>" . $conn->error);
+            }
+    	}
+    	else
+    	{
+    		die("Permission denied");
+    	}
+}
+
+function get_players_from_match($name, $key, $match_id)
+{
+        if ($permission)
+        {
+            $sql = "SELECT players FROM matches WHERE id='" + $match_id + "'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0)
+            {
+                while($row = $result->fetch_assoc())
+                {
+                    $d = $row["players"];
+                    if (!$d == "") {$d = $d + ";";}
+                    return $d;
+                }
+            }
+            else
+            {
+                die("Error match doesnt exist");
+            }
+
+        }
+        else
+        {
+        	die("Permission denied");
+        }
 
 }
 
-function get_new_server_name()
+function add_player_to_match($name, $match_id, $player)
 {
-    return generateRandomString(20);
+    if ($permission)
+    	{
+            $sql = "UPDATE matches SET players='"+get_players_from_match($name, $key, $match_id)+$content+"' WHERE server_name='" + $name + "'";
+            if ($conn->query($sql) === TRUE) {
+                return "OK";
+            } else {
+              die("Error updating: " . $conn->error);
+            }
+    	}
+    	else
+    	{
+    		die("Permission denied");
+    	}
 }
 
-function host_new_server($name)
+function create_new_server()
 {
-	return generateRandomString(20);
+    $name = generateRandomString(20);
+    $sql = "INSERT INTO servers (server_name, key)
+                   VALUES ('"+$name+"', '')";
+
+    if ($conn->query($sql) === TRUE)
+    {
+        return "OK";
+    }
+    else
+    {
+        die("Error: " . $sql . "<br>" . $conn->error);
+    }
+    return $name;
+}
+
+function create_password($name, $key)
+{
+    $new_key = generateRandomString(20);
+    if ($permission)
+    {
+        $sql = "UPDATE servers SET key='"+$new_key+"' WHERE server_name='" + $name + "'";
+        if ($conn->query($sql) === TRUE) {
+            return "OK";
+        } else {
+        die("Error updating: " . $conn->error);
+        }
+        return $new_key;
+    }
+    else
+    {
+        if(!getKeyFromServer($name)
+        {
+            $sql = "UPDATE servers SET key='"+$new_key+"' WHERE server_name='" + $name + "'";
+                    if ($conn->query($sql) === TRUE) {
+                        return "OK";
+                    } else {
+                    die("Error updating: " . $conn->error);
+                    }
+            return $new_key;
+        }
+        else
+        {
+            die("Permission denied");
+        }
+    }
 }
 
 $name = $_GET["server"];
@@ -56,25 +186,29 @@ $command = $_GET["command"];
 
 $data = $_GET["data"];
 
-if($command == "host_new_server")
+$permission = verifyKeyFromServer($name, $key);
+
+if($command == "create_password")
 {
-	$output = host_new_server($name);
+	$output = create_password($name);
 }
 
 if($command == "host_new_match")
 {
-	$output = "";
+	$output = host_new_match($name, $key, $players, $max_players);
 }
 
 if($command == "reload")
 {
-	$output = save_to_database($name, $key, $data);
+	$output = save_to_database($name, $key, $match_id, $data);
 }
 
-if($command == "get_new_server_name")
+if($command == "create_new_server")
 {
-	$output = get_new_server_name();
+	$output = create_new_server();
 }
+
+$conn->close();
 
 echo $output;
 
