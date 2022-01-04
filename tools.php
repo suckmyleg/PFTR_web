b1cdfab7a068ca5095f62054f6584a9b0e221b5c
SuckMyLeg <juanito-146@hotmail.es>
2022-01-04 16:46:44 +0100

    Saving

--- tools.php
+++ tools.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 
 $servername = "localhost";
 $username = "PFTR";
