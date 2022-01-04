f1090ab63b34b2a610e8765efcb2539a7c274183
SuckMyLeg <juanito-146@hotmail.es>
2022-01-04 14:23:25 +0100

    Saving

--- Javascript/players_writer.js
+++ Javascript/players_writer.js
@@ -8,7 +8,7 @@ function write(player, working_directory){
 
 function get_player_html(player, working_directory)
 {
-    return '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" src="'+working_directory+'Images/Hearts/'+player.health+'.png"></div>';
+    return '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" src="'+working_directory+'Images/Hearts/'+parseInt(player.health)+'.png"></div>';
 }
 
 function get_players_html(players, working_directory)
