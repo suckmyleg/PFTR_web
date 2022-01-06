1a90b1dbc5a5412cf4a344d0901be844486a2479
SuckMyLeg <juanito-146@hotmail.es>
2022-01-06 15:25:08 +0100

    Saving

--- Javascript/frame.js
+++ Javascript/frame.js
@@ -5,14 +5,6 @@ function go(frame_id, url)
     frame.src = url;
 }
 
-function go_search_bar(search_bar_id, url, frame_id)
-{
-    var search_bar = document.getElementById(search_bar_id);
-
-    go(frame_id, url + search_bar.innerHTML);
-}
-
-
 function finish_reloading_frame(frame_id, container)
 {
     frame.src = frame.src;
@@ -42,7 +34,51 @@ async function auto_reload_frame(frame_id, delay)
 {
     while (true)
     {
-        await sleep(delay*1000);
-        reload_frame(frame_id);
+        try
+        {
+            await sleep(delay*1000);
+            reload_frame(frame_id);
+        }
+        catch
+        {
+        return;
+        }
     }
+}
+
+function clear_nav(nav_id="nav")
+{
+    document.getElementById(nav_id).innerHTML = "";
+}
+
+// https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function create_search_bar(nav_id="nav", search_bar_id, button_id, frame_url, frame_id=false)
+{
+    var button_bar = document.getElementById(button_id);
+
+    var frame_id = create_new_frame("nav", frame_url, frame_id);
+
+    // https://stackoverflow.com/questions/20941160/javascript-document-getelementbyid-with-onclick
+    button_bar.onclick = function (){
+    console.log(nav_id);
+    var url = document.getElementById(search_bar_id).value;
+    go(frame_id, frame_url + "?m=" + url);
+    };
+}
+
+function create_new_frame(nav_id="nav", frame_url="", frame_id=false)
+{
+    if (!frame_id){
+        frame_id = getRndInteger(0, 10000000) + "_frame"
+    }
+
+    var nav = document.getElementById(nav_id);
+
+    nav.innerHTML += '<div id="frame_container"><iframe id="'+frame_id+'" scrolling="no" src="'+frame_url+'"></iframe></div>';
+
+    return frame_id;
 }
