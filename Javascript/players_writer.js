7f9ef6fc8be8a4350bf217abfa048a73beaff46b
SuckMyLeg <juanito-146@hotmail.es>
2022-01-06 16:25:35 +0100

    Saving

--- Javascript/players_writer.js
+++ Javascript/players_writer.js
@@ -1,35 +1,29 @@
-
-function write(player, working_directory){
-    var screen = document.getElementById("screen");
-
-    screen.innerHTML = screen.innerHTML + get_player_html(player, working_directory);
-
-}
-
 function get_player_html(player, working_directory)
 {
-    return '<div class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" src="'+working_directory+'Images/Hearts/'+parseInt(player.health)+'.png"></div>';
-}
-
-function get_players_html(players, working_directory)
-{
-    var result = "";
-
-    for(var i = 0; i < players.length; i++)
-    {
-        if(players[i].id != "null")
-        {
-            result += get_player_html(players[i], working_directory);
-        }
-    }
-    return result;
+    return '<div id="'+player.id+'" class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" src="'+working_directory+'Images/Hearts/'+parseInt(player.health)+'.png"></div>';
 }
 
 function write_players(players)
 {
     var screen = document.getElementById("screen");
 
-    screen.innerHTML = get_players_html(players, "../");
+    var new_players = "";
+
+    for(var i = 0; i < players.length; i++)
+    {
+        var player_div = document.getElementById(player.id);
+
+        if(players[i].id != "null" && player_div == null)
+        {
+            new_players += get_player_html(players[i], "../");
+        }
+        else
+        {
+            player_div.getElementByClass("hearts").src = +working_directory+"Images/Hearts/"+parseInt(player.health);
+        }
+    }
+
+    screen.innerHTML += new_players;
 }
 
 //https://www.tutorialspoint.com/javascript-sleep-function
