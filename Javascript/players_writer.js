df87ac7a42c01c9457db96063d7eb53846d0402e
SuckMyLeg <juanito-146@hotmail.es>
2022-01-06 19:06:01 +0100

    Saving

--- Javascript/players_writer.js
+++ Javascript/players_writer.js
@@ -1,6 +1,27 @@
 function get_player_html(player, working_directory)
 {
-    return '<div id="'+player.id+'" class="player_container"><div class="player_title_container"><a class="player_name">'+player.name+'</a></div><div><img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'"></div><img class="hearts" id="h'+player.id+'" src="'+working_directory+'Images/Hearts/'+parseInt(player.health)+'.png"></div>';
+    return '<divid="'+player.id+'">'+
+        '<div class="player_container">'+
+            	'<div class="player_display">'+
+            		'<div class="player_title_container">'+
+            			'<a class="player_name">'+
+            				player.name+
+            			'</a>'+
+            		'</div>'+
+            		'<div>'+
+            			'<img class="player_skin" src="https://crafatar.com/renders/body/'+player.id+'">'+
+            		'</div>'+
+            		'<img class="hearts" id="'+player.id+'_hearts" src="'+working_directory+'Images/Hearts/'+parseInt(player.health)+'.png">'+
+            	'</div>'+
+            	'<div class="player_estadistics">'+
+            		'<p>Kills: <a id="'+player.id+'_kills">'+player.number_kills+'</a></p>'+
+            		'<p>Exp_level: <a id="'+player.id+'_exp">'+player.exp_level+'</a></p>'+
+            		'<p>Playing: <a id="'+player.id+'_playing">'+player.time_playing+'</a></p>'+
+            		'<p>Hp: <a id="'+player.id+'_hp">'+player.health+'</a></p>'+
+            		'<div style="height:200px;display:inline-block;"></div>'+
+            	'</div>'+
+            '</div>'
+        '</div>';
 }
 
 function write_players(players)
@@ -23,12 +44,21 @@ function write_players(players)
             else
             {
                 let hearts_src = working_directory + "Images/Hearts/"+parseInt(player.health)+".png";
-                document.getElementById("h"+player.id).src = hearts_src;
+                document.getElementById(player.id+"_hearts").src = hearts_src;
+                document.getElementById(player.id+"_kills").src = player.number_kills;
+                document.getElementById(player.id+"_exp").src = player.exp_level;
+                document.getElementById(player.id+"_playing").src = player.time_playing;
+                document.getElementById(player.id+"_hp").src = player.health;
             }
         }
     }
 
     if (new_players != "") {document.getElementById("screen").innerHTML += new_players;}
+}
+
+function show_stadistics(player_id="")
+{
+    document.getElementById(player_id+"_estadistics");
 }
 
 //https://www.tutorialspoint.com/javascript-sleep-function
