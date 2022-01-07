fda471a6e1366f02839a8c2a397b1e294a63792d
SuckMyLeg <juanito-146@hotmail.es>
2022-01-07 09:50:53 +0100

    Saving

--- players.php
+++ players.php
@@ -7,12 +7,22 @@
 	<script type="text/javascript" src="Javascript/frame.js"></script>
 </head>
 <body onload="auto_reload_frame('players_frame', 30);">
-	<div id="bar_name">
-		<img class="bar_icons" id="icon1" src="Images/Icono/pollo.png">
-		PFTR
-		<img class="bar_icons" id="icon2" src="Images/Icono/aju.png">
-	</div>
-	<hr class="bar_stick">
+	<div id="bar_container">
+    		<div id="bar">
+    			<div id="bar_name">
+    				<img class="bar_icons" id="icon1" src="Images/Icono/pollo.png">
+    				PFTR
+    				<img class="bar_icons" id="icon2" src="Images/Icono/aju.png">
+    			</div>
+    		</div>
+    		<div id="bar_nav">
+            			<a href="index.html">Main page</a>
+            			<a href="players.html">Match</a>
+            			<a href="server.html">Server</a>
+            			<a href="info.html">Info</a>
+            			<a href="servers.php">Servers</a>
+            		</div>
+    	</div>
 	<?php
 
         try {$match_id = $_GET["m"];}
--- servers.php
+++ servers.php
@@ -6,12 +6,22 @@
 	<link rel="stylesheet" type="text/css" href="Styles/frame.css">
 </head>
 <body>
-	<div id="bar_name">
-		<img class="bar_icons" id="icon1" src="Images/Icono/pollo.png">
-		PFTR
-		<img class="bar_icons" id="icon2" src="Images/Icono/aju.png">
-	</div>
-	<hr class="bar_stick">
+	<div id="bar_container">
+    		<div id="bar">
+    			<div id="bar_name">
+    				<img class="bar_icons" id="icon1" src="Images/Icono/pollo.png">
+    				PFTR
+    				<img class="bar_icons" id="icon2" src="Images/Icono/aju.png">
+    			</div>
+    		</div>
+    		<div id="bar_nav">
+            			<a href="index.html">Main page</a>
+            			<a href="players.html">Match</a>
+            			<a href="server.html">Server</a>
+            			<a href="info.html">Info</a>
+            			<a href="servers.php">Servers</a>
+            		</div>
+    	</div>
 	<?php
 	    echo '<div id="servers_frame_container"><iframe id="servers_frame" scrolling="no" src="Screens/servers.php"></iframe></div>';
 	?>
