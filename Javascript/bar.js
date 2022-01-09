2da020c4cff57bf6089d10a419dee59d7d47fa8d
SuckMyLeg <juanito-146@hotmail.es>
2022-01-09 13:33:13 +0100

    Saving

--- Javascript/bar.js
+++ Javascript/bar.js
@@ -2,8 +2,8 @@ function swtich_bar_visibility()
 {
     var bar = document.getElementById("bar_nav_vertical");
 
-    if (bar.style.visibility == "hidden") {bar.style.visibility = "visible"; document.getElementById("div_bar_visible").style.visibility = "visible";}
-    else {bar.style.visibility = "hidden"; document.getElementById("div_bar_visible").style.visibility = "hidden";}
+    if (bar.style.visibility == "visible") {bar.style.visibility = "hidden"; document.getElementById("div_bar_visible").style.visibility = "hidden";}
+    else {bar.style.visibility = "visible"; document.getElementById("div_bar_visible").style.visibility = "visible";}
 }
 
 function hide_bar()
